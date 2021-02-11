0e831e22f10ba317de046f9f30d6d6b97f57664d
Natalia Chamon <nataliachamon@Natalias-MacBook-Pro-2.local>
2021-02-11 12:02:42 +1000

    lbrynth

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = -200f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float speed = 5.0f;
+
+        void Update()
+        {
+            transform.position = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/TimeInSeconds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeInSeconds : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+
+    private void Update()
+    {
+        scoreText.text = player.position.z.ToString();
+    }
+}
+
